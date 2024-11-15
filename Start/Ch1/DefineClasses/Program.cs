79f67fcf009eb3c12cf8085765f20c76dd4bcb60
shunxiangg <s10270399@connect.np.edu.sg>
2024-11-15 15:15:19 +0000

    Advance C#

--- Start/Ch1/DefineClasses/Program.cs
+++ Start/Ch1/DefineClasses/Program.cs
@@ -2,6 +2,13 @@
 // Defining and instantiating classes
 
 // TODO: Create some new Rectangle objects with dimensions
+Rectangle rect1 = new Rectangle(10,20);
+Rectangle rect2 = new Rectangle(30);
 
 
 // TODO: Change the values of width and height
+Console.WriteLine(rect1.GetArea());
+Console.WriteLine(rect2.GetArea())
+
+
+
